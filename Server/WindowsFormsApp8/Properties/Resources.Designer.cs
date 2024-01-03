9b6c1d15b4aefbd7c1a2ebd0f9f8a66a5f225475
DanielSparta <111179755+DanielSparta@users.noreply.github.com>
2024-01-03 06:08:03 +0200

    update

--- Server/WindowsFormsApp8/Properties/Resources.Designer.cs
+++ Server/WindowsFormsApp8/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SpartaRATServer.Properties {
+namespace WindowsFormsApp8.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace SpartaRATServer.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SpartaRATServer.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WindowsFormsApp8.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
